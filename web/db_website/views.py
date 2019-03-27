84961f81cc130599eda9b9d492f42347e1ad04f0
Chad Baily <chadbaily27@gmail.com>
2019-03-27 19:33:18 -0400

    connect to db

--- web/db_website/views.py
+++ web/db_website/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse
 import json
 import requests
+import pymysql
 
 # Create your views here.
 
@@ -11,3 +12,23 @@ import requests
 def home(request):
       # Return the home page
     return render(request, 'home.block.html')
+
+
+def test(request):
+    # Open database connection
+    db = pymysql.connect("database", "root", "example", "project")
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT VERSION()")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print("Database version : %s " % data)
+
+    # disconnect from server
+    db.close()
+
+    return HttpResponse("Database version : %s " % data)
