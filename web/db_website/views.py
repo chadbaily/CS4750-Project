72cca6c22fde6ede3c2841e7221567abfaee7820
Chad Baily <chadbaily27@gmail.com>
2019-03-27 19:17:06 -0400

    basic site

--- web/db_website/views.py
+++ web/db_website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
+import json
+import requests
 
 # Create your views here.
+
+
+def home(request):
+      # Return the home page
+    return render(request, 'home.block.html')
--- web/web/settings.py
+++ web/web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'db_website.apps.DbWebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
