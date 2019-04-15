0c9756801ae69d5b9e42ce2985ae3bc9a6c4aeeb
Chad Baily <chadbaily27@gmail.com>
2019-04-14 22:36:30 -0400

    create

--- web/db_website/urls.py
+++ web/db_website/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^actors/edit/(?P<pk>[0-9]+)/submit/', views.update_actor, name='update_actor'),
     url(r'^actors/edit/(?P<pk>[0-9]+)/$', views.edit_actors, name='edit_actor'),
+    url(r'^actors/create/submit/$', views.submit_create_actor, name='submit_create_actor'),
+    url(r'^actors/create/$', views.create_actor, name='create_actor'),
     url(r'^actors/$', views.actors, name='actors'),
 ]
--- web/db_website/views.py
+++ web/db_website/views.py
@@ -69,8 +69,8 @@ def update_actor(request, pk):
             # ensure the pk is safe
             pk = escape(pk)
 
-            print("UPDATE Actor SET FirstN = '" + first_name + "', MiddleN = '" + middle_name + "', LastN = '" +
-                  last_name + "', Gender = '" + gender + "', DOB = '" + dob + "'  WHERE ActorID = " + pk)
+            # print("UPDATE Actor SET FirstN = '" + first_name + "', MiddleN = '" + middle_name + "', LastN = '" +
+            #       last_name + "', Gender = '" + gender + "', DOB = '" + dob + "'  WHERE ActorID = " + pk)
 
             # Update the DB
             db = pymysql.connect("database", "root", "example", "project")
@@ -83,7 +83,7 @@ def update_actor(request, pk):
                            last_name + "', Gender = '" + gender + "', DOB = '" + dob + "' WHERE ActorID = " + pk)
             # Save the changes
             db.commit()
-            
+
             # disconnect from server
             db.close()
 
@@ -96,3 +96,48 @@ def update_actor(request, pk):
         form = Person()
 
     return HttpResponseRedirect(reverse('edit_actor', args=[pk]))
+
+
+def create_actor(request):
+
+    return render(request, 'create-actor.block.html')
+
+
+def submit_create_actor(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Person(request.POST)
+        # print(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            middle_name = form.cleaned_data['middle_name']
+            gender = form.cleaned_data['gender']
+            dob = form.cleaned_data['dob']
+
+            # Update the DB
+            db = pymysql.connect("database", "root", "example", "project")
+
+            # Prepare to interact with the DB
+            cursor = db.cursor()
+
+            # Insert
+            cursor.execute("INSERT INTO Actor (FirstN, MiddleN, LastN, DOB, Gender) VALUES ('" +
+                           first_name + "','" + middle_name + "','" + last_name + "','" + dob + "','" + gender + "')")
+            # Save the changes
+            db.commit()
+
+            # disconnect from server
+            db.close()
+
+            return HttpResponseRedirect(reverse('actors'))
+        else:
+            # print("form is false")
+            return HttpResponseRedirect(reverse('create_actor'))
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = Person()
+
+    return HttpResponseRedirect(reverse('create_actor'))
