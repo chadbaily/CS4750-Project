1328bf7cfa9d900fcbf3abacaef39c437688cd06
Chad Baily <chadbaily27@gmail.com>
2019-04-24 11:38:58 -0400

    add aws

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7&19lh@6=6it*ochnfkp8g38blf_uo)q=8okrw7fiz6!u^j^@='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'ec2-54-174-22-241.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['localhost', 'ec2-3-92-207-245.compute-1.amazonaws.com']
 
 
 # Application definition
